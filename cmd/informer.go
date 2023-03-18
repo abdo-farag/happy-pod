845d7671d4d3d671ef06d86a78550627c4a31ada
Abdelfadeel Farag <abdofarag85@gmail.com>
2023-03-18 15:02:11 +0100

    feat: use annotation to set egress ports

--- cmd/informer.go
+++ cmd/informer.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"time"
+	"strconv"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
@@ -31,7 +33,7 @@ func NewPodInformer(clientset kubernetes.Interface) (*PodInformer, error) {
 		AddFunc: func(obj interface{}) {
 			pod := obj.(*corev1.Pod)
 			if pod.Annotations[foo] == bar {
-				if err := protect(clientset); err != nil {
+				if err := protect(clientset, pod); err != nil {
 					fmt.Printf("fail to protect pod %v", pod.Name)
 				}
 			}
@@ -55,11 +57,48 @@ func (p *PodInformer) Stop() {
 	close(p.stopCh)
 }
 
-func protect(clientset kubernetes.Interface) error {
+func protect(clientset kubernetes.Interface, pod *corev1.Pod) error {
+	egressPorts := []struct {
+        port     int
+        protocol v1.Protocol
+    }{}
+    if val, ok := pod.Annotations["egress-ports-tcp"]; ok {
+        portStrings := strings.Split(val, ",")
+        for _, portString := range portStrings {
+            port, err := strconv.Atoi(portString)
+            if err != nil {
+                return err
+            }
+            egressPorts = append(egressPorts, struct {
+                port     int
+                protocol v1.Protocol
+            }{
+                port:     port,
+                protocol: v1.ProtocolTCP,
+            })
+        }
+    }
+    if val, ok := pod.Annotations["egress-ports-udp"]; ok {
+        portStrings := strings.Split(val, ",")
+        for _, portString := range portStrings {
+            port, err := strconv.Atoi(portString)
+            if err != nil {
+                return err
+            }
+            egressPorts = append(egressPorts, struct {
+                port     int
+                protocol v1.Protocol
+            }{
+                port:     port,
+                protocol: v1.ProtocolUDP,
+            })
+        }
+    }
+
 	networkPolicy := &networkingv1.NetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "protect-my-pod",
-			Namespace: "default",
+			Name:      "protect-"+pod.Name,
+			Namespace: pod.Namespace,
 		},
 		Spec: networkingv1.NetworkPolicySpec{
 			PodSelector: metav1.LabelSelector{
@@ -79,25 +118,18 @@ func protect(clientset kubernetes.Interface) error {
 							},
 						},
 					},
-					Ports: []networkingv1.NetworkPolicyPort{
-						{
-							Port:     &[]intstr.IntOrString{intstr.FromInt(80)}[0],
-							Protocol: &[]v1.Protocol{v1.ProtocolTCP}[0],
-						},
-						{
-							Port:     &[]intstr.IntOrString{intstr.FromInt(443)}[0],
-							Protocol: &[]v1.Protocol{v1.ProtocolTCP}[0],
-						},
-						{
-							Port:     &[]intstr.IntOrString{intstr.FromInt(53)}[0],
-							Protocol: &[]v1.Protocol{v1.ProtocolUDP}[0],
-						},
-					},
+					Ports: []networkingv1.NetworkPolicyPort{},
 				},
 			},
 		},
 	}
 
+	for _, port := range egressPorts {
+        networkPolicy.Spec.Egress[0].Ports = append(networkPolicy.Spec.Egress[0].Ports, networkingv1.NetworkPolicyPort{
+            Port:     &[]intstr.IntOrString{intstr.FromInt(port.port)}[0],
+            Protocol: &port.protocol,
+        })
+    }
 	if _, err := clientset.NetworkingV1().NetworkPolicies("default").Create(
 		context.Background(), networkPolicy, metav1.CreateOptions{},
 	); err != nil {
